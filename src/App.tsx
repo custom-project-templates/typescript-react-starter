2bd5ddd7034f4cb312ac0675a945546de24c1ec0
xhmm <super.xhmm@gmail.com>
2019-07-22 12:16:22 +0800

    add App.tsx for better folder structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+import Index from './pages';
+
+const App: FunctionComponent<IProps> = ({}) => {
+  return (
+    <>
+      <Index />
+    </>
+  );
+};
+
+interface IProps {}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './pages/Index';
+import App from './App';
 
-ReactDOM.render(<Index />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
